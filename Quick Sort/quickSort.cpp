c60abdbb0cb28286910b02d57ecbf0f065b0271f
Taylor Huang <asd00012334@hotmail.com>
2016-10-10 13:03:13 +0800

    Update quickSort.cpp

--- Quick Sort/quickSort.cpp
+++ Quick Sort/quickSort.cpp
@@ -4,12 +4,7 @@ using namespace std;
 
 template<typename iter>
 void quickSort(iter begin, iter end){
-    static int randGenerated = 0;
-    int size = end-begin;
-    if(size<=1) return;
-    if(!randGenerated) srand(time(NULL));
-    randGenerated = (randGenerated+1)%50000;
-    swap(begin[0],begin[rand()%size]);
+    if(end-begin<=1) return;
     iter form = begin+1;
     iter latt = begin+size-1;
     while(form<=latt)

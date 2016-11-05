8167248b88c7dd14baf7949f263da19bc2802238
asd00012334 <asd00012334@hotmail.com>
2016-11-05 14:16:49 +0800

    Submitted

---
+++ Multi-Threading Merge Sort/thrdMergeSort.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename iter>
+void thrdMergeSort(iter begin, iter end){
+    int size=end-begin;
+    if(size<=1) return;
+    iter mid=begin+size/2;
+    thread lThread(thrdMergeSort<iter>,begin,mid),rThread(thrdMergeSort<iter>,mid,end);
+    lThread.join();
+    rThread.join();
+    iter form = begin;
+    iter latt = mid;
+    typename iterator_traits<iter>::value_type tempArr[end-begin];
+    for(int cnt=0;cnt<sizeof(tempArr);cnt++)
+        if(form!=mid && latt!=end)
+            if(*form<*latt)
+                tempArr[cnt] = *(form++);
+            else
+                tempArr[cnt] = *(latt++);
+        else if(form != mid)
+            tempArr[cnt] = *(form++);
+        else if(latt != end)
+            tempArr[cnt] = *(latt++);
+    for(iter cnt=begin; cnt!=end ; ++cnt)
+        *cnt = tempArr[cnt-begin];
+}
+
+
+
+int main(){
+    string str="This is a Multi-Threading Merge Sort";
+    thrdMergeSort(str.begin(),str.end());
+    cout<<str<<endl;
+    return 0;
+}

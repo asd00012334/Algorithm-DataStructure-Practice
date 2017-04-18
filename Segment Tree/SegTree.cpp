b431dc8c6112893e9d656bba6b0f1dfe2699268c
asd00012334 <asd00012334@hotmail.com>
2017-04-19 01:38:31 +0800

    Reconstruction
    Lazy Tag

--- Segment Tree/SegTree.cpp
+++ Segment Tree/SegTree.cpp
@@ -1,123 +1,89 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 template<typename type>
 class SegTree{
-    static type const& (*operate)(type const&,type const&);
-    int rootIdx;
-    size_t length;
-    struct Node{
-        type val;
-        int idx;
-        int begin;
-        int end;
-        int left;
-        int right;
-        Node(){}
-        Node(type val, int idx):
-            val(val),idx(idx){}
-    };
-    vector<Node> storage;
+    inline int left(int idx){return idx*2+1;}
+    inline int right(int idx){return idx*2+2;}
+    int segLen;
+    vector<type> storage;
+    vector<bool> tags;
+    type multiply(type base, int size){
+        type result = base;
+        --size;
+        while(size){
+            if(size&1) result = result+base;
+            base = base+base;
+            size/=2;
+        }
+        return result;
+    }
+    void pushDown(int root, int size){
+        storage[left(root)] = storage[right(root)] = storage[root];
+        tags[left(root)] = tags[right(root)] = true;
+        storage[root] = multiply(storage[root], size);
+        tags[root] = false;
+    }
 public:
-    SegTree(){
-        length = 0;
-        rootIdx = EOF;
-        operate = NULL;
-    }
+    SegTree(){}
 
     template<typename iter>
-    SegTree(iter begin, iter end, type const& (*operate)(type const&,type const&) = max){
-        build(begin,end, operate);
+    SegTree(iter begin, iter end){
+        build(begin, end);
     }
 
-    size_t const& size(){return length;}
-
-    void clear(){
-        length = 0;
-        storage.clear();
-        rootIdx = EOF;
-        operate = NULL;
-    }
+    inline int length(){return segLen;}
 
     template<typename iter>
-    Node build(iter begin, iter end, type const& (*operate)(type const&,type const&) = max, iter initial = (iter)NULL){
-        if(initial==(iter)NULL){
-            storage.clear();
-            initial = begin;
-            SegTree::operate = operate;
-            length=0;
-        }
+    void build(iter begin, iter end, int root = 0){
         int size = end-begin;
-        if(size == 1){
-            length++;
-            storage.push_back(Node(*begin,storage.size()));
-            storage.back().begin = begin - initial;
-            storage.back().end = end - initial;
-            storage.back().left = storage.back().right = EOF;
-            rootIdx = storage.back().idx;
-            return storage.back();
+        if(!root){
+            int heapLen = pow(2,floor(log2(size*2+2))+1)-1;
+            segLen = size;
+            storage.resize(heapLen);
+            tags = vector<bool>(heapLen, false);
         }
-        Node left = build(begin, begin+size/2,operate,initial);
-        Node right = build(begin+size/2,end,operate,initial);
-        storage.push_back(Node(operate(left.val,right.val),storage.size()));
-        storage.back().begin = begin - initial;
-        storage.back().end = end - initial;
-        storage.back().left = left.idx;
-        storage.back().right = right.idx;
-        rootIdx = storage.back().idx;
-        return storage.back();
-    }
-
-    type query(int begin ,int end, int curIdx = EOF){
-        if(curIdx == EOF) curIdx = rootIdx;
-        Node& cur = storage[curIdx];
-        int size = cur.end - cur.begin;
-        if(size<0) throw exception();
-        if(size==0) return type();
-        if(begin<cur.begin||cur.end<end) throw exception();
-        if(cur.begin == begin && cur.end == end) return cur.val;
-        if(end<=cur.begin+size/2) return query(begin,end,cur.left);///left
-        if(cur.begin+size/2<=begin) return query(begin,end,cur.right);///right
-        return operate(
-                   query(begin,storage[cur.left].end,cur.left),
-                   query(storage[cur.right].begin,end,cur.right)
-                   );
-    }
-
-    void update(int begin, int end, type val, int curIdx = EOF){
-        if(curIdx == EOF) curIdx = rootIdx;
-        Node& cur = storage[curIdx];
-        int size = cur.end - cur.begin;
-        if(size<0) throw exception();
-        if(size==0) return;
-        if(begin<cur.begin||cur.end<end) throw exception();
-        if(size==1){
-            cur.val = val;
+        if(size == 1){
+            storage[root] = *begin;
             return;
         }
-        if(end<=cur.begin+size/2)
-            update(begin,end,val,cur.left);
-        else if(cur.begin+size/2<=begin)
-            update(begin,end,val,cur.right);
-        else{
-            update(begin,storage[cur.left].end,val,cur.left);
-            update(storage[cur.right].begin,end,val,cur.right);
-        }
-        cur.val = operate(storage[cur.left].val,storage[cur.right].val);
+        build(begin, begin+size/2,left(root));
+        build(begin+size/2, end, right(root));
+        storage[root] = storage[left(root)] + storage[right(root)];
     }
-
+    type query(int begin, int end, int root=0, int lower=0, int upper=0){
+        if(!root) upper = segLen;
+        int size=upper-lower;
+        if(size==1) return storage[root];
+        if(tags[root]) pushDown(root,size);
+        if(begin==lower && end==upper) return storage[root];
+        if(end<=lower+size/2) return query(begin, end, left(root), lower, lower+size/2);
+        if(lower+size/2<=begin) return query(begin,end, right(root), lower+size/2,upper);
+        return query(begin, lower+size/2, left(root), lower, lower+size/2) + query(lower+size/2, end, right(root), lower+size/2, upper);
+    }
+    type update(int begin, int end, type val, int root=0, int lower=0, int upper=0){
+        if(!root) upper = segLen;
+        int size=upper-lower;
+        if(size==1) return storage[root]=val;
+        if(tags[root]) pushDown(root,size);
+        if(begin==lower && end==upper){
+            storage[root]=val;
+            tags[root]=true;
+            return multiply(val,size);
+        }
+        if(end<=lower+size/2) return storage[root] = update(begin, end, val, left(root), lower, lower+size/2) + storage[right(root)];
+        if(lower+size/2<=begin) return storage[root] = storage[left(root)] + update(begin,end, val, right(root), lower+size/2,upper);
+        return storage[root] = update(begin, lower+size/2, val, left(root), lower, lower+size/2) + update(lower+size/2, end, val, right(root), lower+size/2, upper);
+    }
 };
 
-template<typename type>
-type const& (*SegTree<type>::operate)(type const&,type const&);
 
 int main(){
-    string str = "This is a segment tree.";
-    SegTree<char> seg;
-    seg.build(str.begin(),str.end());
-    for(int cnt=0;cnt<seg.size();cnt++)
-        cout<<seg.query(cnt,cnt+1);
-    cout<<endl;
+    vector<int> arr(50,1);
+    SegTree<int> t(arr.begin(),arr.end());
+    t.update(0,50,2);
+    cout<<t.query(11,25)<<endl;
     return 0;
 }
+

423e70fb339efe48634f9365f4b641bd818564e4
Taylor Huang <asd00012334@hotmail.com>
2017-06-23 16:16:41 +0800

    Update FFT.cpp

--- Fast Fourier Transform/FFT.cpp
+++ Fast Fourier Transform/FFT.cpp
@@ -1,54 +1,65 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-template<typename type>
-void substitute(vector<complex<type> > const& seq, complex<type> omega){
-    if(seq.size()==1) return;
-    vector<complex<type> > odd(seq.size()/2);
-    vector<complex<type> > even(seq.size()-odd.size());
-    for(int cnt=0;cnt<seq.size();cnt++){
-        if(cnt%2) odd[cnt/2]=seq[cnt];
-        else even[cnt/2]=seq[cnt];
-    }
-    complex<type> w2=omega*omega;
-    substitute(even,w2);
-    substitute(odd,w2);
-    for(int cnt=0;cnt<seq.size();cnt++)
-        seq[cnt]=even[cnt/2]+omega*odd[cnt/2];
+template<class Cont>
+void fft(Cont& x){
+    typedef typename Cont::value_type type;
+    int size = x.size();
+    if(size==1 || size==0) return;
+    if(size&(size-1)) x.resize(size = 1<<__lg(size<<1),0);
+    Cont even(size>>1), odd(size+1>>1);
+    for(int n=0;n<size;n++)
+        if(n&1) odd[n>>1] = x[n];
+        else even[n>>1] = x[n];
+    fft(even);
+    fft(odd);
+    const double pi = acos(-1);
+    for(int k=0;k<size;k++)
+        x[k] = (
+            even[k%even.size()]+
+            exp(-type(0,-k*2*pi/size)*odd[k%odd.size()])
+        )/type(2);
 }
 
-template<typename type>
-void fourier(vector<complex<type> >& seq){
-    if(seq.empty()) return;
-    vector<complex<type> > out(seq.size());
-    seq.resize(pow(2,ceil(log2(seq.size()))),0);
-    complex<type> i(0,1);
-    complex<type> pi(acos(0)*2,0);
-    complex<type> omega(exp(-i*2*pi/seq.size()));
-    substitute(seq,omega);
-    type base=sqrt(seq.size());
-    for(int cnt=0;cnt<seq.size();cnt++)
-        seq[cnt]/=base;
+template<class Cont>
+void ifft(Cont& x){
+    typedef typename Cont::value_type type;
+    int size = x.size();
+    if(size==1 || size==0) return;
+    if(size&(size-1)) x.resize(size = 1<<__lg(size<<1),0);
+    Cont even(size>>1), odd(size+1>>1);
+    for(int n=0;n<size;n++)
+        if(n&1) odd[n>>1] = x[n];
+        else even[n>>1] = x[n];
+    fft(even);
+    fft(odd);
+    const double pi = acos(-1);
+    for(int k=0;k<size;k++)
+        x[k] = (
+            even[k%even.size()]+
+            exp(type(0,-k*2*pi/size)*odd[k%odd.size()])
+        );
 }
-
-template<typename type>
-void invFourier(vector<complex<type> >& seq){
-    if(seq.empty()) return;
-    vector<complex<type> > out(seq.size());
-    seq.resize(pow(2,ceil(log2(seq.size()))),0);
-    complex<type> i(0,1);
-    complex<type> pi(acos(0)*2,0);
-    complex<type> omega(exp(i*2*pi/seq.size()));
-    substitute(seq,omega);
-    type base=sqrt(seq.size());
-    for(int cnt=0;cnt<seq.size();cnt++)
-        seq[cnt]/=base;
-}
-
 
 int main(){
-    /** Experimental **/
-
+    double Ts = 0.001;
+    double T = 100;
+    double const pi = acos(-1);
+    int N = T/Ts;
+    vector<double> x(N);
+    vector<double> y(N);
+    for(int cnt=0;cnt<N;cnt++){
+        x[cnt] = cnt*Ts;
+        y[cnt] = cos(2*pi*x[cnt]*5);
+    }
+    vector<complex<double> > f(y.begin(),y.end());
+    for(int cnt=0;cnt<10;cnt++)
+        cout<<y[cnt]<<" ";
+    cout<<endl;
+    fft(f);
+    ifft(f);
+    for(int cnt=0;cnt<10;cnt++)
+        cout<<y[cnt]<<" ";
     return 0;
 }

6492497dd5788b5a23c31569cc1d75edc22af5fb
Taylor Huang <asd00012334@hotmail.com>
2017-07-13 00:22:22 +0800

    Update LCS.cpp

--- LCS/LCS.cpp
+++ LCS/LCS.cpp
@@ -1,24 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-template<typename container>
-int lengthLCS(container const& left, container const& right){
-    int LCS[left.size()+1][right.size()+1];
-    for(int l = 0;l<=left.size(); l++)
-    for(int r = 0;r<=right.size(); r++)
-        if(!l||!r) LCS[l][r] = 0;
-        else
-            if(!(left[l-1]<right[r-1])&&!(right[r-1]<left[l-1]))
-                LCS[l][r] = LCS[l-1][r-1]+1;
-            else
-                LCS[l][r] = max(LCS[l-1][r], LCS[l][r-1]);
-    return LCS[left.size()][right.size()];
+int LCS(vector<int>& l, vector<int>& r){
+    vector<int> length(r.size()+1,0);
+    for(int row=1;row<=l.size();row++)
+    for(int col=1;col<=r.size();col++){
+        if(l[row-1]==r[col-1]) length[col]=length[col-1]+1;
+        else length[col] = max(length[col],length[col-1]);
+    }
+    return length[r.size()];
 }
 
 int main(){
     vector<int> a = {9,3,6,7,2,4};
     vector<int> b = {7,9,2,4,6};
-    cout<<lengthLCS(a,b)<<endl;
+    cout<<LCS(a,b)<<endl;
     return 0;
 }

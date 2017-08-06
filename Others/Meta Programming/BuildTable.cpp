1db3011d7f9d0dd203b0b7a550ff3c0e6663b0eb
Taylor Huang <asd00012334@hotmail.com>
2017-08-06 20:53:54 +0800

    Update BuildTable.cpp

--- Others/Meta Programming/BuildTable.cpp
+++ Others/Meta Programming/BuildTable.cpp
@@ -1,21 +1,23 @@
 #include<bits/stdc++.h>
-
+#define TableSize 800
 using namespace std;
 
 /// C<n,k>::val
-template<int n, int k> struct C{ static const int val = C<n-1,k>::val+C<n-1,k-1>::val;};
-template<int n> struct C<n,0>{ static const int val=1;};
-template<int k> struct C<k,k>{ static const int val=1;};
+template<int n, int k> struct C{ static const int val;};
+template<int n> struct C<n,0>{ static const int val;};
+template<int n, int k> const int C<n,k>::val = C<n,k-1>::val*(n-k+1)/k;
+template<int n> const int C<n,0>::val=1;
 
-/// Table<length,n,0>::arr[k]
-template<int length, int n, int k> struct Table: Table<length, n, k-1>{ static const int dummy;};
-template<int length, int n> struct Table<length,n,0>{ static int const dummy; static int arr[length];};
-template<int length, int n, int k> const int Table<length,n,k>::dummy = Table<length,n,0>::arr[k] = C<n,k>::val+Table<length,n,k-1>::dummy*0;
-template<int length, int n> int Table<length,n,0>::arr[length];
-template<int length, int n> const int Table<length,n,0>::dummy = Table<length,n,0>::arr[0] = C<n,0>::val;
-template struct Table<5,5,5>;
+
+/// Table<n,0>::arr[k]
+template<int n, int k> struct Table: Table<n, k-1>{ static const int dummy;};
+template<int n> struct Table<n,0>{ static int const dummy; static int arr[n];};
+template<int n, int k> const int Table<n,k>::dummy = Table<n,0>::arr[k] = C<n,k>::val+Table<n,k-1>::dummy*0;
+template<int n> int Table<n,0>::arr[n];
+template<int n> const int Table<n,0>::dummy = Table<n,0>::arr[0] = C<n,0>::val;
+template struct Table<TableSize,TableSize>;
 
 int main(){
-    cout<<C<5,3>::val<<"\n";
-    cout<<Table<5,5,0>::arr[3]<<"\n";
+    cout<<C<TableSize,3>::val<<"\n";
+    cout<<Table<TableSize,TableSize>::arr[3]<<"\n";
 }

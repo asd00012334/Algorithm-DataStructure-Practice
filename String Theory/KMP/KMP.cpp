d14a7433f49b0022088966d2a7c8502014dfabf6
Taylor Huang <asd00012334@hotmail.com>
2017-08-09 05:55:51 +0800

    Update KMP.cpp

--- String Theory/KMP/KMP.cpp
+++ String Theory/KMP/KMP.cpp
@@ -1,40 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-
-int KMP(string const& T, string const& P){
-    if(T.size()<P.size()||T.size()==0||P.size()==0) return EOF;
-    int txt, pat;
-    vector<int> prefix(P.size());
-    pat=1;
-    prefix[0]=0;
-    while(pat<P.size()){
-        int pre=prefix[pat-1];
-        while(pre>0&&P[pre]!=P[pat])
-            pre = prefix[pre-1];
-        prefix[pat] = pre;
-        if(P[pre]==P[pat])
-            prefix[pat]++;
-        pat++;
+/// Yet to be tested
+vector<int> fail;
+void build(string const& str){
+    fail = vector<int>(str.size(),-1);
+    for(int cnt=1;cnt<str.size();++cnt){
+        int pivot = fail[cnt-1];
+        while(pivot!=-1 && str[pivot+1]!=str[cnt])
+            pivot = fail[pivot];
+        if(str[pivot+1]==str[cnt]) fail[cnt]=pivot+1;
+        else fail[cnt]=-1;
     }
-    txt=pat=0;
-    while(txt<T.size()&&pat<P.size()){
-        if(T[txt]==P[pat]){
-            txt++;
-            pat++;
-        }
-        else{
-            if(pat) pat=prefix[pat-1];
-            else txt++;
+}
+bool KMP(string const& s, string const& p, vector<int>& match){
+    build(p);
+    match = vector<int>();
+    int pos=-1;
+    for(int cnt=0;cnt<s.size();++cnt){
+        if(s[cnt]==p[pos+1]) ++pos;
+        else if(pos!=-1) pos=fail[pos], --cnt;
+        if(pos==p.size()-1){
+            match.push_back(cnt);
+            pos = fail[pos];
         }
     }
-    if(pat==P.size()) return txt-pat;
-    return EOF;
 }
 
 
-
 int main(){
-    cout<<KMP("This is a KMP algorithm.","is is")<<endl;
-    return 0;
+    string s("aaabaabbabb"), p("aa");
+    vector<int> match;
+    KMP(s,p,match);
+    for(int cnt=0;cnt<match.size();++cnt)
+        printf("%d ",match[cnt]);
+    printf("\n");
 }

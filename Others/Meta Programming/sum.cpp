e15db30d93f66fd12aad965685a24fcce5a04191
Taylor Huang <asd00012334@hotmail.com>
2017-08-31 20:38:18 +0800

    Update sum.cpp

--- Others/Meta Programming/sum.cpp
+++ Others/Meta Programming/sum.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 
+using namespace std;
+
 /// Sum out const-size non-empty array
 /// i.e. sum<5, int>::get(arr) = arr[0]+arr[1]+...+arr[4]
 template<int size, typename type>

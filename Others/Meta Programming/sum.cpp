1ff8e1cff9dd1196d6f38208d5f36edd77402d53
Taylor Huang <asd00012334@hotmail.com>
2017-08-31 20:37:08 +0800

    Create sum.cpp

---
+++ Others/Meta Programming/sum.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+/// Sum out const-size non-empty array
+/// i.e. sum<5, int>::get(arr) = arr[0]+arr[1]+...+arr[4]
+template<int size, typename type>
+struct sum{
+    static inline type get(type const arr[]){
+        return *arr+sum<size-1,type>::get(arr+1);
+    }
+};
+
+template<typename type>
+struct sum<0,type>{
+    static inline type get(type const arr[]){
+        return type();
+    }
+};
+
+int main(){
+    return 0;
+}

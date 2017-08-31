164a70e57e9bcd9e882dca1f969f4f617fe10be7
Taylor Huang <asd00012334@hotmail.com>
2017-08-31 20:38:53 +0800

    Update NdArray.cpp

--- Data Structure/NdArray/NdArray.cpp
+++ Data Structure/NdArray/NdArray.cpp
@@ -14,22 +14,6 @@ struct addStar<0,type>{
     typedef type ptr;
 };
 
-/// Sum out const-size non-empty array
-/// i.e. sum<5, int>::get(arr) = arr[0]+arr[1]+...+arr[4]
-template<int size, typename type>
-struct sum{
-    static inline type get(type const arr[]){
-        return *arr+sum<size-1,type>::get(arr+1);
-    }
-};
-
-template<typename type>
-struct sum<0,type>{
-    static inline type get(type const arr[]){
-        return type();
-    }
-};
-
 /// Calculate total size of the memory pool for an ND-Array
 template<int dim, typename type>
 struct memSize{

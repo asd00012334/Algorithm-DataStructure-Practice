b57257a9b91f4cdd2a2b7d25fb3d1edb7db0048a
Taylor Huang <asd00012334@hotmail.com>
2018-10-06 00:20:21 +0800

    Create TarjanSCC.cpp

---
+++ Graph Theory/SCC/TarjanSCC.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n,m;
+    while(cin>>n>>m){
+        if(!n && !m) break;
+        vector<vector<int> > adjList(n);
+        for(int i=0;i<m;++i){
+            int u,v; scanf("%d%d",&u,&v);
+            --u, --v;
+            adjList[u].push_back(v);
+        }
+
+        /// Tarjan's SCC
+        int t = 0;
+        vector<int> vis(n,0), stk, root(n);
+        vector<int> onChain(n,0), low(n), in(n);
+        function<int(int)> dfs = [&](int u){
+            low[u] = in[u] = t++;
+            onChain[u] = 1;
+            stk.push_back(u);
+            int sccN = 0;
+            for(int i=0;i<adjList[u].size();++i){
+                int v = adjList[u][i];
+                if(!vis[v]){
+                    vis[v] = 1;
+                    sccN += dfs(v);
+                }
+                if(onChain[v]){
+                    /// backward edge: update time flag of scc
+                    low[u] = min(low[v],low[u]);
+                }
+            }
+            if(in[u]==low[u]){
+                /// u is the root of scc
+                ++sccN;
+                while(stk.size()){
+                    int v = stk.back();
+                    stk.pop_back();
+                    if(in[u]!=low[v]) break;
+                    root[v] = u;
+                    onChain[v] = 0;
+                }
+            }
+            return sccN;
+        };
+        int sccN = 0;
+        for(int i=0;i<n;++i){
+            if(vis[i]) continue;
+            vis[i] = 1;
+            sccN += dfs(i);
+        }
+        if(sccN==1) puts("Yes");
+        else puts("No");
+    }
+    return 0;
+}
